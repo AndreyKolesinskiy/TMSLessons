2211d74af074ba75e9dbbe44b550d3a1a90f8125
Andrei Kaliasinski <andykolesinski@gmail.com>
2023-12-18 01:08:46 +0300

    Added Hello to User

--- TMSLesson1/Program.cs
+++ TMSLesson1/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello there,  please write your name and press Enter!");
+String userName = Console.ReadLine();
+Console.WriteLine("Hello, " + userName + "! Nice to meet you!");
