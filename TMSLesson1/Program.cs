1eec349d30c39a3e44177eff00cb3b49136fb6eb
Andrei Kaliasinski <andykolesinski@gmail.com>
2023-12-21 07:18:57 +0300

    Tets change

--- TMSLesson1/Program.cs
+++ TMSLesson1/Program.cs
@@ -1,4 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello there,  please write your name and press Enter!");
-String userName = Console.ReadLine();
-Console.WriteLine("Hello, " + userName + "! Nice to meet you!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello there,  please write your name and press Enter!");
+        string userName = Console.ReadLine();
+        Console.WriteLine("Hello, " + userName + "! Nice to meet you!!");
+    }
+}
