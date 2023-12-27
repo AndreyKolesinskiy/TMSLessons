25ee02aa80eded11d44a6bb704ac0bd71a4191bd
Andrei Kaliasinski <andykolesinski@gmail.com>
2023-12-27 07:49:31 +0300

    Added first function

--- TMSLesson1/Program.cs
+++ TMSLesson1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        function1();
+    }
+    static void function1()
+    {
+        Console.WriteLine("Enter first number");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter second number");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int result = a;
+
+        for (int i = 1; i < b; i++)
+        {
+            result *= a;
+        }
+
+        Console.WriteLine("a^b=" + result);
+    }
+}
