f82b05841539776104fca6b39f72f7dbfec751ad
Andrei Kaliasinski <andykolesinski@gmail.com>
2023-12-28 09:41:45 +0300

    Exercises 5 and 6

--- TMSLesson1/Program.cs
+++ TMSLesson1/Program.cs
@@ -4,7 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        function4();
+        function6();
     }
     static void function1()
     {
@@ -40,7 +40,7 @@ internal class Program
         int a = Convert.ToInt32(Console.ReadLine());
         int result = 0;
 
-        for (int i=1; Math.Pow(i,2) <a; i++)
+        for (int i = 1; Math.Pow(i, 2) < a; i++)
         {
             result++;
         }
@@ -54,11 +54,55 @@ internal class Program
         int a = Convert.ToInt32(Console.ReadLine());
         int result = 0;
 
-        for (int i = 1; i < a; i ++)
+        for (int i = 1; i < a; i++)
         {
             if (a % i == 0)
                 result = i;
         }
         Console.WriteLine(result);
     }
+
+    static void function5()
+    {
+        Console.WriteLine("Enter A and B numbers");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int result = 0;
+
+        if (a <= b)
+        {
+            for (int i = a; i <= b; i++)
+            {
+                if (i % 7 == 0) result+=i;
+            }
+        }
+        else
+        {
+            for (int i = b; i <= a; i++)
+            {
+                if (i % 7 == 0) result += i;
+            }
+        }
+
+        Console.WriteLine(result);
+    }
+
+    static void function6()
+    {
+        Console.WriteLine("Enter positive number");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int previousNumber = 1;
+        int currentNumber = 1;
+        int numberBeforePrevious = 1;
+
+        for(int i = 3; i <= a; i++)
+        {
+            currentNumber = previousNumber +numberBeforePrevious;
+            numberBeforePrevious = previousNumber;
+            previousNumber = currentNumber;
+        }
+
+        Console.WriteLine(currentNumber);
+    }
+
 }
