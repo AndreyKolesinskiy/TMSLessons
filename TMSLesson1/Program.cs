1f8830fe84f04959e7cfd036e506b7a2be1a7fec
Andrei Kaliasinski <andykolesinski@gmail.com>
2024-01-15 07:53:34 +0300

    All exercises

--- TMSLesson1/Program.cs
+++ TMSLesson1/Program.cs
@@ -1,7 +1,76 @@
-﻿internal class Program
+﻿using System.Text;
+using TMSLesson1;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        function5();
+    }
+
+    static void function1()
+    {
+        Console.WriteLine("Enter a sentence containinf 'test' and some numbers"); // Smoke test 1a, test 2b should be done as soon as possible.
+        string initialSentence = Console.ReadLine();
+        initialSentence = initialSentence.Replace("test", "testing");
+        StringBuilder sentenceWithountDigits = new StringBuilder();
+
+        foreach (char value in initialSentence)
+        {
+            if (!char.IsDigit(value))
+            {
+                sentenceWithountDigits.Append(value);
+            }
+        }
+        Console.WriteLine(sentenceWithountDigits);
+    }
+
+    static void function2()
+    {
+        string phrase = "Welcome to the TMS lessons";
+        string[] arr = phrase.Split(new char[] { ' ' });
+        string newPhrase = "";
+        foreach (string value in arr)
+        {
+            newPhrase += "\"" + value + "\" ";
+        }
+        Console.WriteLine(newPhrase);
+    }
+
+    static void function3()
+    {
+        string initialString = "teamwithsomeofexcersicesabcwanttomakeitbetter";
+        string searchElement = "abc";
+        string partBeforeAbc = initialString.Remove(initialString.IndexOf(searchElement));
+        string partAfterAbc = initialString.Substring(initialString.IndexOf(searchElement) + searchElement.Length);
+
+        Console.WriteLine(partBeforeAbc);
+        Console.WriteLine(partAfterAbc);
+    }
+
+    static void function4()
+    {
+        string initialString = "Bad day";
+        string removedBad = initialString.Substring(4);
+        string addedNice = removedBad.Insert(0, "Nice ") + "!!!!!!!!!";
+        string replacedLastExclamationMark = addedNice.Remove(addedNice.LastIndexOf("!"));
+        replacedLastExclamationMark += "?";
+
+        Console.WriteLine(removedBad);
+        Console.WriteLine(addedNice);
+        Console.WriteLine(replacedLastExclamationMark);
+    }
+
+    static void function5()
+    {
+        Console.WriteLine("Enter document number in next format: xxxx-yyy-xxxx-yyy-xyxy where x is digit and y is character");
+        string documentNumber = Console.ReadLine(); // Example: "1234-abc-5678-def-1a2b"
+        Task5Methods.function5_1(documentNumber);
+        Task5Methods.function5_2(documentNumber);
+        Task5Methods.function5_3(documentNumber);
+        Task5Methods.function5_4(documentNumber);
+        Task5Methods.function5_5(documentNumber);
+        Task5Methods.function5_6(documentNumber);
+        Task5Methods.function5_7(documentNumber);
     }
 }
---
+++ TMSLesson1/Task5Methods.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMSLesson1
+{
+    internal class Task5Methods
+    {
+        public static void function5_1(string documentNumber)
+        {
+            string[] arr = documentNumber.Split(new char[] { '-' });
+            Console.WriteLine(arr[0] + arr[2]);
+        }
+
+        public static void function5_2(string documentNumber)
+        {
+            string[] arr = documentNumber.Split(new char[] { '-' });
+            Console.WriteLine(arr[0] + "-***" + "-" + arr[2] + "-***" + "-" + arr[4]);
+        }
+
+        public static void function5_3(string documentNumber)
+        {
+            documentNumber = documentNumber.Replace("-", "");
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char value in documentNumber)
+            {
+                if (!char.IsDigit(value))
+                {
+                    sb.Append(value);
+                }
+            }
+            string result = sb.ToString().ToLower();
+
+            Console.WriteLine(result.Substring(0, 3) + "/" + result.Substring(3, 3)
+                + "/" + result.Substring(6, 1) + "/" + result.Substring(7, 1));
+        }
+
+        public static void function5_4(string documentNumber)
+        {
+            documentNumber = documentNumber.Replace("-", "");
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char value in documentNumber)
+            {
+                if (!char.IsDigit(value))
+                {
+                    sb.Append(value);
+                }
+            }
+            string result = sb.ToString().ToUpper();
+
+            Console.WriteLine(result.Substring(0, 3) + "/" + result.Substring(3, 3)
+                + "/" + result.Substring(6, 1) + "/" + result.Substring(7, 1));
+        }
+
+        public static void function5_5(string documentNumber)
+        {
+            string element = "abc";
+            string result = documentNumber.ToLower().Contains(element) ? "Document conains abc or ABC" : "Document doesn't contain abc and ABC";
+            Console.Write(result);
+        }
+
+        public static void function5_6(string documentNumber)
+        {
+            string element = "555";
+            string result = documentNumber.StartsWith(element) ? "Document starts with " + element : "Document doesn't start with " + element;
+            Console.Write(result);
+        }
+
+        public static void function5_7(string documentNumber)
+        {
+            string element = "1a2b";
+            string result = documentNumber.EndsWith(element) ? "Document ends with " + element : "Document doesn't end with " + element;
+            Console.Write(result);
+        }
+    }
+}
