f6bd12a1bc71f106f94120ab93cd4a147bd1ec6d
Andrei Kaliasinski <andykolesinski@gmail.com>
2024-01-17 07:53:57 +0300

    Square exercise

--- TMSLesson1/Program.cs
+++ TMSLesson1/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using TMSLesson1;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Square greenSquare = new Square(5, "Green");
+        Square darkBlueSquare = new Square(2, "Dark Blue");
+        Square lightBlueSquare = new Square(15, "Light Blue");
+
+        greenSquare.printInfoAboutSquare();
+        darkBlueSquare.printInfoAboutSquare();
+        lightBlueSquare.printInfoAboutSquare();
+        greenSquare.sideSize = darkBlueSquare.sideSize * 3;
+
+        greenSquare.printInfoAboutSquare();
     }
 }
---
+++ TMSLesson1/Square.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMSLesson1
+{
+    internal class Square
+    {
+        public int sideSize;
+        public string squareColor;
+        public int squareSpace;
+
+        public Square(int sideSize, string squareColor) 
+        {
+            this.sideSize = sideSize;
+            this.squareColor = squareColor;
+            calculateSquareSpace(sideSize);
+        }
+
+        public int calculateSquareSpace(int sideSize)
+        {
+             return squareSpace = sideSize*sideSize;
+        }
+
+        public void printInfoAboutSquare()
+        {
+            Console.WriteLine("Side size is " + sideSize);
+            Console.WriteLine("Square color is " + squareColor);
+            Console.WriteLine("Square space is " + calculateSquareSpace(sideSize));
+            Console.WriteLine();
+        }
+    }
+}
