94919760ffa40d6298dda8df892b9e598d242226
Andrei Kaliasinski <andykolesinski@gmail.com>
2024-01-21 08:23:38 +0300

    Clinic terminal

---
+++ TMSLesson1/Therapist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TMSLesson1
+{
+    public class Therapist : Doctor
+    {
+        public Therapist(string name, int visitCost, string qualification, int id) : base(name, visitCost, qualification, id) { }
+        public string specialization = "Therapist";
+
+        public override string GetSpecilaization()
+        {
+            return specialization;
+        }
+        public override void Treat()
+        {
+            Console.WriteLine(specialization + " " + name + " treats...");
+        }
+    }
+}
