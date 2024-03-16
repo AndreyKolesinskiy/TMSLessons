aaa41740cea9cb3a7657bbea3a2cb1ebea11506b
Andrei Kaliasinski <andykolesinski@gmail.com>
2024-03-16 16:53:54 +0300

    Refactoring

--- TMSLesson1/Program.cs
+++ TMSLesson1/Program.cs
@@ -2,8 +2,6 @@
 
 internal class Program
 {
-    public delegate void SayGoodbye();
-    public static event SayGoodbye DoctorSayGoodbye;
     private static void Main(string[] args)
     {
         Clinic clinic = new Clinic();
